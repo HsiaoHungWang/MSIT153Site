7396c153c241bf16162abd9eec354c8965b00932
HsiaoHungWang <wang@ispan.com.tw>
2023-11-14 15:29:25 +0800

    新增 apiController

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MSIT153Site.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return Content("<h2>Ajax 你好 !!</h2>","text/html", System.Text.Encoding.UTF8);
+            return Content("Hello Ajax!!");
+        }
+    }
+}
