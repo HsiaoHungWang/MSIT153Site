30726c483a13417a2ff6a37996e49b66c781edbd
HsiaoHungWang <wang@ispan.com.tw>
2023-11-14 16:40:16 +0800

    透過ViewModel接收資料

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -1,17 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MSIT153Site.Models.ViewModel;
 
 namespace MSIT153Site.Controllers
 {
     public class ApiController : Controller
     {
-        public IActionResult Index(string name)
+        //  public IActionResult Index(string name, int age=30)
+        public IActionResult Index(UserViewModel user)
         {
-            if (string.IsNullOrEmpty(name))
+            if (string.IsNullOrEmpty(user.name))
             {
-                name = "guest";
+                user.name = "guest";
             }
             //return Content("<h2>Ajax 你好 !!</h2>","text/html", System.Text.Encoding.UTF8);
-            return Content($"Hello {name}!!");
+            return Content($"Hello {user.name}， You are {user.age} years old.");
         }
     }
 }
---
+++ Models/ViewModel/UserViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MSIT153Site.Models.ViewModel
+{
+    public class UserViewModel
+    {
+        public string? name { get; set; }
+        public int? age { get; set; }
+    }
+}
