21f61f70fe9052e0336f8ab87ef0e2ad913ec906
HsiaoHungWang <wang@ispan.com.tw>
2023-11-15 14:50:30 +0800

    IFormFile接收檔案，IWebHostEnvironment取得實際路徑

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MSIT153Site.Models.ViewModel;
+using System.IO;
 
 namespace MSIT153Site.Controllers
 {
     public class ApiController : Controller
     {
+        private readonly IWebHostEnvironment _host;
+        public ApiController(IWebHostEnvironment host)
+        {
+            _host = host;
+        }
         //  public IActionResult Index(string name, int age=30)
         public IActionResult Index(UserViewModel user)
         {
@@ -17,11 +23,19 @@ namespace MSIT153Site.Controllers
             return Content($"Hello {user.name}， You are {user.age} years old.");
         }
 
-        public IActionResult register(MemberViewModel member)
+        public IActionResult register(MemberViewModel member, IFormFile formFile)
         {
-            
+            //實際路徑
+            //C:\Users\User\Documents\workspace\MSIT153Site\wwwroot\uploads\abc.jpg
+            //string strPath = _host.ContentRootPath; //C:\Users\User\Documents\workspace\MSIT153Site
+            //tring strPath = _host.WebRootPath; //C:\Users\User\Documents\workspace\MSIT153Site\wwwroot
+            string strPath = Path.Combine(_host.WebRootPath, "uploads", formFile.FileName);
+            return Content(strPath);
+
+            //string fileInfo = $"{formFile?.FileName} - {formFile?.Length} - {formFile?.ContentType}";
+            //return Content(fileInfo);
             //return Content("<h2>Ajax 你好 !!</h2>","text/html", System.Text.Encoding.UTF8);
-            return Content($"Hello {member.name}，{member.email},  You are {member.age} years old.");
+            //return Content($"Hello {member.name}，{member.email},  You are {member.age} years old.");
         }
     }
 }
--- Models/ViewModel/MemberViewModel.cs
+++ Models/ViewModel/MemberViewModel.cs
@@ -5,5 +5,7 @@
         public string? name { get; set; }
         public string? email { get; set; }
         public int? age { get; set; }
+
+       // public IFormFile? fileName { get; set; }
     }
 }
